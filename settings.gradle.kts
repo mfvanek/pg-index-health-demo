a573a6636bd6e66ea95bf142bf2ee243e5e9f702
i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>
2023-06-01 19:27:58 +0400

    Migrated to Gradle version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "pg-index-health-demo"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("postgresql", "org.postgresql:postgresql:42.6.0")
+            library("logback-classic", "ch.qos.logback:logback-classic:1.4.7")
+            val pgIndexHealth = version("pg-index-health", "0.9.3")
+            library("pgIndexHealth-core", "io.github.mfvanek", "pg-index-health")
+                    .versionRef(pgIndexHealth)
+            library("pgIndexHealth-generator", "io.github.mfvanek", "pg-index-health-generator")
+                    .versionRef(pgIndexHealth)
+            library("pgIndexHealth-testing", "io.github.mfvanek", "pg-index-health-testing")
+                    .versionRef(pgIndexHealth)
+        }
+    }
+}
