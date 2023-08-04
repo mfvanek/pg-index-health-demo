8e2ca1fc77fcb70f46c97b85c2ce4bfeae826666
i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>
2023-08-04 15:36:27 +0400

    Updated Gradle to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import org.sonarqube.gradle.SonarTask
 
 plugins {
     id("java")
-    id("com.github.spotbugs") version "5.0.14"
+    id("com.github.spotbugs") version "5.1.1"
     id("checkstyle")
     id("jacoco")
     id("pmd")
