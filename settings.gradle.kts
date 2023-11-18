5ae343659764b0b36d9021c5428a3f03eb952949
Ivan Vakhrushev <mfvanek@gmail.com>
2023-11-18 14:39:55 +0400

    Usage of BOM

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,19 +5,6 @@ dependencyResolutionManagement {
         create("libs") {
             library("postgresql", "org.postgresql:postgresql:42.6.0")
             library("logback-classic", "ch.qos.logback:logback-classic:1.4.11")
-            val pgIndexHealth = version("pg-index-health", "0.10.0")
-            library("pg-index-health-core", "io.github.mfvanek", "pg-index-health")
-                    .versionRef(pgIndexHealth)
-            library("pg-index-health-generator", "io.github.mfvanek", "pg-index-health-generator")
-                    .versionRef(pgIndexHealth)
-            library("pg-index-health-testing", "io.github.mfvanek", "pg-index-health-testing")
-                    .versionRef(pgIndexHealth)
-            version("checkstyle", "10.12.0")
-            version("pmd", "6.55.0")
-            version("jacoco", "0.8.10")
-            library("pitest-dashboard-reporter", "it.mulders.stryker:pit-dashboard-reporter:0.2.1")
-            version("pitest-junit5Plugin", "1.2.0")
-            version("pitest-core", "1.15.0")
         }
     }
 }
