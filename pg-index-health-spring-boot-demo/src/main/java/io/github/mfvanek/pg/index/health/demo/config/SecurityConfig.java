c3268900e22acc1226dadb1c4327872ea7930e65
Ivan Vakhrushev <mfvanek@gmail.com>
2024-10-12 22:11:40 +0400

    Refactored test #2

--- pg-index-health-spring-boot-demo/src/main/java/io/github/mfvanek/pg/index/health/demo/config/SecurityConfig.java
+++ pg-index-health-spring-boot-demo/src/main/java/io/github/mfvanek/pg/index/health/demo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     @SneakyThrows
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) {
         return http
+            .csrf(AbstractHttpConfigurer::disable) //NOSONAR
             .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults())
             .build();
