c1e396672e9c0f0f5a381b5317a2cd479da416b1
Ivan Vakhrushev <mfvanek@gmail.com>
2025-04-20 23:03:47 +0400

    Fix linter

--- db-migrations/src/main/resources/db/changelog/sql/dictionary.sql
+++ db-migrations/src/main/resources/db/changelog/sql/dictionary.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 
 --changeset ivan.vakhrushev:2025.04.11:dictionary.table
+-- noqa: disable=RF05
 create table if not exists demo."dictionary-to-delete"
 (
     "dict-id" bigint not null generated always as identity
