227d279e6f89e0146b930a776ebbe818bfdc91c4
Ivan Vakhrushev <mfvanek@gmail.com>
2025-06-26 19:04:28 +0400

    Update dependencies

--- buildSrc/src/main/kotlin/pg-index-health-demo.pitest.gradle.kts
+++ buildSrc/src/main/kotlin/pg-index-health-demo.pitest.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 dependencies {
     testImplementation("org.junit.platform:junit-platform-launcher")
 
-    pitest("it.mulders.stryker:pit-dashboard-reporter:0.3.4")
+    pitest("it.mulders.stryker:pit-dashboard-reporter:0.3.5")
 }
 
 pitest {
