06579fac8017b1065dfd628c131dab74a42dbda8
oninbo <oninbo@gmail.com>
2025-09-08 16:22:33 +0300

    Add Kotlin Spring Boot demo project with configuration and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "pg-index-health-demo"
 
 include("pg-index-health-demo-without-spring")
 include("pg-index-health-spring-boot-demo")
+include("pg-index-health-spring-boot-kotlin-demo")
 include("db-migrations")
 
 dependencyResolutionManagement {
