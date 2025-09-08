229726059f9002cf68ace19dd33e73e71c399811
oninbo <oninbo@gmail.com>
2025-09-08 16:26:35 +0300

    Add DbHealthController integration test for health endpoint validation

---
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DbHealthControllerTest.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-demo
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.index.health.demo.kotlin.controller
+
+import io.github.mfvanek.pg.index.health.demo.kotlin.utils.BasePgIndexHealthDemoSpringBootTest
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+
+class DbHealthControllerTest : BasePgIndexHealthDemoSpringBootTest() {
+
+    @Test
+    fun shouldCollectHealthData() {
+        webTestClient.get()
+            .uri("/db/health")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(this::setUpBasicAuth)
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(String::class.java)
+    }
+}
