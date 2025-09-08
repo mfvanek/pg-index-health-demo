262554b19098b4c5bd9467226b60226d054722bd
oninbo <oninbo@gmail.com>
2025-09-08 16:34:30 +0300

    Add Kotlin test for ForeignKeyJsonDeserializer with validation cases

--- pg-index-health-spring-boot-demo/src/test/java/io/github/mfvanek/pg/index/health/demo/config/ForeignKeyJsonDeserializerTest.java
+++ pg-index-health-spring-boot-demo/src/test/java/io/github/mfvanek/pg/index/health/demo/config/ForeignKeyJsonDeserializerTest.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-// TODO: add similar test to kotlin demo and other tests too
 class ForeignKeyJsonDeserializerTest extends BasePgIndexHealthDemoSpringBootTest {
 
     @Autowired
---
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/config/ForeignKeyJsonDeserializerTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-demo
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.index.health.demo.kotlin.config
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.mfvanek.pg.index.health.demo.kotlin.utils.BasePgIndexHealthDemoSpringBootTest
+import io.github.mfvanek.pg.model.constraint.ForeignKey
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+
+class ForeignKeyJsonDeserializerTest : BasePgIndexHealthDemoSpringBootTest() {
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Test
+    @Throws(JsonProcessingException::class)
+    fun deserializeShouldWork() {
+        val original = ForeignKey.ofNotNullColumn("users", "fk_user_role", "role_id")
+        val json = objectMapper.writeValueAsString(original)
+        val restored = objectMapper.readValue(json, ForeignKey::class.java)
+        assertThat(restored)
+            .isEqualTo(original)
+    }
+
+    @Test
+    fun deserializeShouldThrowExceptionWhenNoColumns() {
+        val json = """{"tableName":"users","constraintName":"fk_user_role","constraintType":"FOREIGN_KEY","name":"fk_user_role","objectType":"CONSTRAINT","validateSql":"alter table users validate constraint fk_user_role;"}"""
+
+        assertThatThrownBy { objectMapper.readValue(json, ForeignKey::class.java) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("columnsInConstraint cannot be empty")
+    }
+
+    @Test
+    fun deserializeShouldThrowExceptionWhenColumnsIsNotArray() {
+        val json = """{"tableName":"users","constraintName":"fk_user_role","columnsInConstraint":"test","constraintType":"FOREIGN_KEY","name":"fk_user_role","objectType":"CONSTRAINT","validateSql":"alter table users validate constraint fk_user_role;"}"""
+
+        assertThatThrownBy { objectMapper.readValue(json, ForeignKey::class.java) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("columnsInConstraint cannot be empty")
+    }
+}
