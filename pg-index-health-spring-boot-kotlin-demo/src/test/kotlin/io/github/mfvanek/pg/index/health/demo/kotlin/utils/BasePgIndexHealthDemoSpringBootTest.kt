629bc77cc7cc02c40430f28523c883a87ba92156
oninbo <oninbo@gmail.com>
2025-09-08 16:25:51 +0300

    Add Kotlin Spring Boot demo tests with basic auth and logging config

---
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/utils/BasePgIndexHealthDemoSpringBootTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-demo
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.index.health.demo.kotlin.utils
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalManagementPort
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import java.time.Clock
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+abstract class BasePgIndexHealthDemoSpringBootTest {
+
+    @Autowired
+    protected lateinit var jdbcTemplate: JdbcTemplate
+
+    @LocalServerPort
+    protected var port: Int = 8080
+
+    @LocalManagementPort
+    protected var actuatorPort: Int = 8090
+
+    @Autowired
+    protected lateinit var clock: Clock
+
+    @Autowired
+    protected lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    private lateinit var securityProperties: SecurityProperties
+
+    protected fun setUpBasicAuth(httpHeaders: HttpHeaders) {
+        httpHeaders.setBasicAuth(securityProperties.user.name, securityProperties.user.password)
+    }
+}
