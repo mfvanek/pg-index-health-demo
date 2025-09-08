005d16c84f46fe0c07f33bb020eb2bacffd867e1
oninbo <oninbo@gmail.com>
2025-09-08 16:25:51 +0300

    Add Kotlin Spring Boot demo tests with basic auth and logging config

---
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DefaultControllerTest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-demo
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.index.health.demo.kotlin.controller
+
+import io.github.mfvanek.pg.index.health.demo.kotlin.utils.BasePgIndexHealthDemoSpringBootTest
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import java.util.Locale
+import kotlin.test.assertNull
+
+class DefaultControllerTest : BasePgIndexHealthDemoSpringBootTest() {
+
+    @Test
+    fun rootPageShouldRedirectToSwaggerUi() {
+        val result = webTestClient.get()
+            .uri("/")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(this::setUpBasicAuth)
+            .exchange()
+            .expectStatus().isFound
+            .expectHeader().location(String.format(Locale.ROOT, "http://localhost:%d/actuator/swagger-ui", port))
+            .expectBody()
+            .returnResult()
+            .responseBody
+        assertNull(result)
+    }
+}
