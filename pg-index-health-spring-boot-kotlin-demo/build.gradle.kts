bb1437691ee6620464606649d1a5462477598f5a
oninbo <oninbo@gmail.com>
2025-09-10 13:22:19 +0300

    chore(build): increase pitest mutation threshold and add kotlin internal packages to avoid calls to

--- pg-index-health-spring-boot-kotlin-demo/build.gradle.kts
+++ pg-index-health-spring-boot-kotlin-demo/build.gradle.kts
@@ -108,5 +108,13 @@ pitest {
     excludedTestClasses.set(listOf("io.github.mfvanek.pg.index.health.demo.kotlin.ActuatorEndpointTest")) // TODO: add ActuatorEndpointTest class
     // Lower the mutation threshold for Kotlin demo to allow build to pass
     // TODO: Improve test coverage to meet the standard threshold
-    mutationThreshold.set(55)
+    mutationThreshold.set(76)
+    
+    // Prevent Pitest from removing calls to Kotlin internal methods
+    avoidCallsTo.set(
+        listOf(
+            "kotlin.jvm.internal",
+            "kotlin.jdk7"
+        )
+    )
 }
