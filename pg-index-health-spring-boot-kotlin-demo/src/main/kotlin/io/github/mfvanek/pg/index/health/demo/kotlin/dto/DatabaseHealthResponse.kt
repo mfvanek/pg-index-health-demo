268e1a12e82e04a6ed59730f08af69472489c231
oninbo <oninbo@gmail.com>
2025-09-11 16:56:34 +0300

    Add Swagger schema annotations to response DTOs for improved API documentation

--- pg-index-health-spring-boot-kotlin-demo/src/main/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/dto/DatabaseHealthResponse.kt
+++ pg-index-health-spring-boot-kotlin-demo/src/main/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/dto/DatabaseHealthResponse.kt
@@ -7,6 +7,13 @@
 
 package io.github.mfvanek.pg.index.health.demo.kotlin.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Response containing database health information")
 data class DatabaseHealthResponse(
+    @field:Schema(
+        description = "List of health data entries in format 'check_name:count'",
+        example = """["bloated_tables:0", "tables_with_missing_indexes:0", "tables_without_primary_key:1", "duplicated_indexes:2", "foreign_keys_without_index:5", "bloated_indexes:0", "indexes_with_null_values:1", "intersected_indexes:3", "invalid_indexes:1", "unused_indexes:8", "tables_without_description:0", "columns_without_description:1", "columns_with_json_type:0", "columns_with_serial_types:0", "functions_without_description:0", "indexes_with_boolean:1", "not_valid_constraints:1", "btree_indexes_on_array_columns:1", "sequence_overflow:1", "primary_keys_with_serial_types:1", "duplicated_foreign_keys:1", "intersected_foreign_keys:0", "possible_object_name_overflow:1", "tables_not_linked_to_others:0", "foreign_keys_with_unmatched_column_type:1", "tables_with_zero_or_one_column:0", "objects_not_following_naming_convention:2", "columns_not_following_naming_convention:1", "primary_keys_with_varchar:0", "columns_with_fixed_length_varchar:12", "indexes_with_unnecessary_where_clause:0", "primary_keys_that_most_likely_natural_keys:0", "columns_with_money_type:0", "indexes_with_timestamp_in_the_middle:0"]"""
+    )
     val healthData: List<String>
 )
