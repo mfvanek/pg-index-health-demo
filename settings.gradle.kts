bf530e2bdbc65749debf9fe0840a001c338a77bc
oninbo <oninbo@gmail.com>
2025-09-17 11:58:22 +0300

    Migrate springdoc-openapi to version catalog with 2.8.13 update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,11 +8,14 @@ include("db-migrations")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
+            val springDocVersion = version("springdoc-openapi", "2.8.13")
             val springBoot3Version = version("spring-boot-v3", "3.5.5")
             plugin("spring-boot-v3", "org.springframework.boot")
                 .versionRef(springBoot3Version)
             library("spring-boot-v3-dependencies", "org.springframework.boot", "spring-boot-dependencies")
                 .versionRef(springBoot3Version)
+            library("springdoc-openapi-bom", "org.springdoc", "springdoc-openapi-bom")
+                .versionRef(springDocVersion)
             library("liquibase-core", "org.liquibase:liquibase-core:4.33.0")
             library("liquibase-sessionlock", "com.github.blagerweij:liquibase-sessionlock:1.6.9")
         }
