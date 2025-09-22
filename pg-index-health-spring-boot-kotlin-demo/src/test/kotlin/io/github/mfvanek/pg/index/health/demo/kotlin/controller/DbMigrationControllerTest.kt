5553306e0e3ccb272287e4b335acf760340cd61a
oninbo <oninbo@gmail.com>
2025-09-22 18:25:21 +0300

    Add mock test for migration error handling and refactor controller tests

--- pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DbMigrationControllerTest.kt
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DbMigrationControllerTest.kt
@@ -7,152 +7,59 @@
 
 package io.github.mfvanek.pg.index.health.demo.kotlin.controller
 
-import io.github.mfvanek.pg.health.checks.common.DatabaseCheckOnCluster
-import io.github.mfvanek.pg.index.health.demo.kotlin.dto.ForeignKeyColumnDto
-import io.github.mfvanek.pg.index.health.demo.kotlin.dto.ForeignKeyDto
 import io.github.mfvanek.pg.index.health.demo.kotlin.dto.ForeignKeyMigrationResponse
 import io.github.mfvanek.pg.index.health.demo.kotlin.dto.MigrationError
 import io.github.mfvanek.pg.index.health.demo.kotlin.utils.BasePgIndexHealthDemoSpringBootTest
-import io.github.mfvanek.pg.model.constraint.ForeignKey
-import io.github.mfvanek.pg.model.context.PgContext
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.test.context.bean.override.mockito.MockitoBean
-import kotlin.test.assertEquals
+import org.springframework.web.util.UriBuilder
 import kotlin.test.assertTrue
+
+import org.assertj.core.api.Assertions.assertThat
 
 class DbMigrationControllerTest : BasePgIndexHealthDemoSpringBootTest() {
 
-    @MockitoBean
-    private lateinit var foreignKeysNotCoveredWithIndex: DatabaseCheckOnCluster<ForeignKey>
-
-    @BeforeEach
-    fun setUp() {
-        `when`(foreignKeysNotCoveredWithIndex.check(any(PgContext::class.java)))
-            .thenReturn(listOf(ForeignKey.ofNotNullColumn("test_table", "fk_col", "col_name")))
-            .thenReturn(emptyList())
-    }
-
     @Test
-    fun shouldGenerateMigrationsWithForeignKeysChecked() {
-        val result = webTestClient.post()
-            .uri("/db/migration/generate")
+    fun runsMigrations() {
+        val result = webTestClient
+            .post()
+            .uri { uriBuilder: UriBuilder? ->
+                uriBuilder!!
+                    .pathSegment("db", "migration", "generate")
+                    .build()
+            }
             .accept(MediaType.APPLICATION_JSON)
-            .headers(this::setUpBasicAuth)
+            .headers { httpHeaders: HttpHeaders? -> this.setUpBasicAuth(httpHeaders!!) }
             .exchange()
-            .expectStatus().isOk
-            .expectBody(ForeignKeyMigrationResponse::class.java)
+            .expectStatus().isOk()
+            .expectBody<ForeignKeyMigrationResponse?>(ForeignKeyMigrationResponse::class.java)
             .returnResult()
-            .responseBody
+            .getResponseBody()
 
         assertTrue(result != null)
-
-        assertEquals(1, result.foreignKeysBefore.size, "getForeignKeysBefore() should return non-empty list")
-        assertEquals(0, result.foreignKeysAfter.size, "getForeignKeysAfter() should return empty list")
-        assertTrue(result.generatedMigrations.isNotEmpty(), "getGeneratedMigrations() should return non-empty list")
-
-        assertEquals(
-            "test_table",
-            result.foreignKeysBefore[0].tableName,
-            "getForeignKeysBefore() should return the actual list"
-        )
-        assertTrue(
-            result.generatedMigrations.all { it.contains("create index concurrently if not exists") },
-            "getGeneratedMigrations() should return the actual list"
-        )
+        assertThat(result.foreignKeysBefore).isNotEmpty()
+        assertThat(result.foreignKeysAfter).isEmpty()
+        assertThat(result.generatedMigrations).allMatch { s -> s.contains("create index concurrently if not exists") }
     }
 
     @Test
     fun returnsNothingWithWrongAuthorization() {
-        val result = webTestClient.post()
-            .uri("/db/migration/generate")
+        val result = webTestClient
+            .post()
+            .uri { uriBuilder: UriBuilder? ->
+                uriBuilder!!
+                    .pathSegment("db", "migration", "generate")
+                    .build()
+            }
             .accept(MediaType.APPLICATION_JSON)
             .exchange()
             .expectStatus().isEqualTo(HttpStatus.UNAUTHORIZED)
             .expectBody(MigrationError::class.java)
             .returnResult()
-            .responseBody
+            .getResponseBody()
 
-        assertEquals(result, null)
-    }
-
-    @Test
-    fun handleMigrationExceptionShouldReturnExpectedError() {
-        val mockForeignKey = ForeignKey.ofNotNullColumn("demo.test_table", "fk_test_column", "test_column")
-
-        `when`(foreignKeysNotCoveredWithIndex.check(any(PgContext::class.java)))
-            .thenReturn(emptyList())
-            .thenReturn(listOf(mockForeignKey))
-
-        val result = webTestClient.post()
-            .uri("/db/migration/generate")
-            .accept(MediaType.APPLICATION_JSON)
-            .headers(this::setUpBasicAuth)
-            .exchange()
-            .expectStatus().isEqualTo(HttpStatus.EXPECTATION_FAILED)
-            .expectBody(MigrationError::class.java)
-            .returnResult()
-            .responseBody
-
-        assertTrue(result != null)
-        assertEquals(result.statusCode, HttpStatus.EXPECTATION_FAILED.value())
-        assertEquals(result.message.contains("Migrations failed:"), true)
-
-        verify(foreignKeysNotCoveredWithIndex, times(2)).check(any(PgContext::class.java))
-    }
-
-    @Test
-    fun shouldDirectlyTestForeignKeyMigrationResponseDtoWithNonEmptyValues() {
-        val foreignKeyDto1 = ForeignKeyDto(
-            tableName = "table1",
-            constraintName = "fk_col1",
-            columns = listOf(ForeignKeyColumnDto(name = "ref_col1", nullable = false))
-        )
-        val foreignKeyDto2 = ForeignKeyDto(
-            tableName = "table2",
-            constraintName = "fk_col2",
-            columns = listOf(ForeignKeyColumnDto(name = "ref_col2", nullable = false))
-        )
-        val foreignKeysBefore = listOf(foreignKeyDto1, foreignKeyDto2)
-        val foreignKeysAfter = listOf(foreignKeyDto1)
-        val generatedMigrations = listOf("CREATE INDEX idx1 ON table1 (col1);", "CREATE INDEX idx2 ON table2 (col2);")
-
-        val response = ForeignKeyMigrationResponse(foreignKeysBefore, foreignKeysAfter, generatedMigrations)
-
-        assertEquals(2, response.foreignKeysBefore.size, "getForeignKeysBefore() should return non-empty list")
-        assertEquals(1, response.foreignKeysAfter.size, "getForeignKeysAfter() should return non-empty list")
-        assertEquals(2, response.generatedMigrations.size, "getGeneratedMigrations() should return non-empty list")
-
-        assertEquals(
-            "table1",
-            response.foreignKeysBefore[0].tableName,
-            "getForeignKeysBefore() should return the actual list"
-        )
-        assertEquals(
-            "table2",
-            response.foreignKeysBefore[1].tableName,
-            "getForeignKeysBefore() should return the actual list"
-        )
-        assertEquals(
-            "table1",
-            response.foreignKeysAfter[0].tableName,
-            "getForeignKeysAfter() should return the actual list"
-        )
-        assertEquals(
-            "CREATE INDEX idx1 ON table1 (col1);",
-            response.generatedMigrations[0],
-            "getGeneratedMigrations() should return the actual list"
-        )
-        assertEquals(
-            "CREATE INDEX idx2 ON table2 (col2);",
-            response.generatedMigrations[1],
-            "getGeneratedMigrations() should return the actual list"
-        )
+        assertThat(result).isNull()
     }
 }
