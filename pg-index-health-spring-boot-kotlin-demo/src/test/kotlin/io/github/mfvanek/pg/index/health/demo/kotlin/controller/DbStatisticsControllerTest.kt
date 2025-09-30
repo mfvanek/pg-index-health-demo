247f00d16b83c217d677a1c3d15e8c4bb4e7a8d5
oninbo <oninbo@gmail.com>
2025-09-30 23:43:29 +0300

    Move mock-based tests to separate DbStatisticsControllerMockTest class

--- pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DbStatisticsControllerTest.kt
+++ pg-index-health-spring-boot-kotlin-demo/src/test/kotlin/io/github/mfvanek/pg/index/health/demo/kotlin/controller/DbStatisticsControllerTest.kt
@@ -7,27 +7,15 @@
 
 package io.github.mfvanek.pg.index.health.demo.kotlin.controller
 
-import io.github.mfvanek.pg.health.checks.management.DatabaseManagement
 import io.github.mfvanek.pg.index.health.demo.kotlin.dto.StatisticsResetResponse
 import io.github.mfvanek.pg.index.health.demo.kotlin.utils.BasePgIndexHealthDemoSpringBootTest
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito.`when`
 import org.springframework.boot.test.system.OutputCaptureExtension
 import org.springframework.http.MediaType
-import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @ExtendWith(OutputCaptureExtension::class)
 class DbStatisticsControllerTest : BasePgIndexHealthDemoSpringBootTest() {
-
-    @MockitoBean
-    private lateinit var databaseManagement: DatabaseManagement
-
-    @BeforeEach
-    fun setUp() {
-        `when`(databaseManagement.resetStatistics()).thenReturn(true)
-    }
 
     @Test
     fun shouldGetLastResetDate() {
@@ -62,20 +50,5 @@ class DbStatisticsControllerTest : BasePgIndexHealthDemoSpringBootTest() {
             .exchange()
             .expectStatus().isAccepted
             .expectBody(StatisticsResetResponse::class.java)
-    }
-
-    @Test
-    fun shouldThrowExceptionWhenResetStatisticsWithoutWaitFails() {
-        `when`(databaseManagement.resetStatistics()).thenReturn(false)
-
-        webTestClient.post()
-            .uri("/db/statistics/reset")
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(false)
-            .headers(this::setUpBasicAuth)
-            .exchange()
-            .expectStatus().is5xxServerError
-            .expectBody()
-            .jsonPath("$.message").isEqualTo("Statistics reset failed: Could not reset statistics")
     }
 }
